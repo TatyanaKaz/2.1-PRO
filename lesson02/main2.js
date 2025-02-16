6b683a1ce4ea8e43f4369f29a34ab7bc06520b01
TatyanaKaz <tat.kazakovceva@gmail.com>
2025-02-16 19:59:54 +0300

    added folder with lesson2

--- lesson02/main2.js
+++ lesson02/main2.js
@@ -1,8 +1,9 @@
+let lesson2 = '2'
 const firstName = 'Татьяна'
 let city = 'Киров'
 let hobbies = 'webflow'
 
-
+console.log( 'Урок №', lesson2);
 console.log( 'Меня зовут', firstName);
 console.log( 'Я живу в городе', city);
 console.log( 'Мое увлечение', hobbies);
