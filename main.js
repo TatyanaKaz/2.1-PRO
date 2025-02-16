21cd1362b65c7726a7e5c3f7df41d05af117fe2c
TatyanaKaz <tat.kazakovceva@gmail.com>
2025-02-16 15:32:01 +0300

    Описание, что изменилось

---
+++ main.js
@@ -0,0 +1,8 @@
+const firstName = 'Татьяна'
+let city = 'Киров'
+let hobbies = 'webflow'
+
+
+console.log( 'Меня зовут', firstName);
+console.log( 'Я живу в городе', city);
+console.log( 'Мое увлечение', hobbies);
