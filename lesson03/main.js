955319910eb29aa7de070e7bca0f3176282d3f2d
TatyanaKaz <tat.kazakovceva@gmail.com>
2025-02-19 22:09:04 +0300

    lesson3

---
+++ lesson03/main.js
@@ -0,0 +1,10 @@
+let titleProject = 'Оценка стоимости';
+let screensValue = 'шаблонные, с уникальным дизайном, с анимациями';
+let screenPrice = 1500;
+let percentage = 200;
+let fullPrice = 80000;
+let responsive = true;
+
+
+
+
